c3ec307e145379873b273c2e23017fabc58c2c2e
nikhilsrinath <nikhilsrinath7@gmail.com>
2025-09-03 10:43:57 +0530

    Totally Changed

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,14 +1,25 @@
-/** @type {import('tailwindcss').Config} */
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
+  mode: 'jit',
+  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {
       fontFamily: {
-        sans: ['San Francisco', 'sans-serif'],
-      }
+        primary: ['Inter', ...fontFamily.sans],
+      },
+      colors: {
+        primary: {
+          400: '#00E0F3',
+          500: '#00c4fd',
+        },
+        dark: '#333333',
+      },
     },
   },
-  plugins: [],
-}
+  variants: {
+    extend: {},
+  },
+  plugins: [require('@tailwindcss/forms')],
+};
