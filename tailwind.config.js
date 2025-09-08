3bd2d6505c65c1f05a3e9795ded518074cb10861
nikhilsrinath <nikhilsrinath7@gmail.com>
2025-09-08 19:33:32 +0530

    All error done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,3 @@
-<<<<<<< HEAD
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-  content: [
-    "./src/**/*.{js,jsx,ts,tsx}",
-  ],
-  theme: {
-    extend: {
-      fontFamily: {
-        sans: ['San Francisco', 'sans-serif'],
-      }
-    },
-  },
-  plugins: [],
-}
-=======
 export default {
   content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
@@ -26,4 +10,3 @@ export default {
   },
   plugins: [],
 };
->>>>>>> sugan
