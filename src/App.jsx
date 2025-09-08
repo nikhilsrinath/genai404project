2582f9a139b65dcbdcbdebeef1d2b25012a05170
nikhilsrinath <nikhilsrinath7@gmail.com>
2025-09-08 10:38:17 +0530

    Ai PAge added

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import UserDetails from "./pages/UserDetails";
 import ProfessionPage from "./pages/ProffesionPage";
+import AiPage from "./pages/AiPage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         <Route path="/login" element={<Login />} />
         <Route path="/userdetails" element={<UserDetails />} />
         <Route path="/profession" element={<ProfessionPage />} />
+        <Route path="/soul-ai" element={<AiPage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/AiPage.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AiPage = () => {
+  return (
+    <div>AiPage</div>
+  )
+}
+
+export default AiPage
